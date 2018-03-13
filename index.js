d4d2ccdacd9e26430d043a812a4eb80fa9fc0f6e
Luke Burns <lukemburns@gmail.com>
2018-03-12 23:06:11 -0400

    load only if mounted

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ if (window && window.MutationObserver) {
       eachMutation(mutations[i].removedNodes, function (index, el) {
         if (!document.documentElement.contains(el)) turnoff(index, el)
       })
-      eachMutation(mutations[i].addedNodes, turnon)
+      eachMutation(mutations[i].addedNodes, function (index, el) {
+        if (document.documentElement.contains(el)) turnon(index, el)
+      })
     }
   })
   if (document.body) {
